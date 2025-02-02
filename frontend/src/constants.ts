ae97154dd46fd6e3007ad2a34eadb536352ca1a8
Ray Lim <rayjlim@yahoo.com>
2025-02-01 23:54:50 -0800

    fix: add 'national park'

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
 export const ENVIRONMENT = import.meta.env.MODE;
 export const FULL_DATE_FORMAT = 'yyyy-MM-dd';
 export const REST_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-export const TAGS = ['watch', 'hike', 'animals', 'completed', 'archived', 'water park', 'event', 'food', 'trang', 'wonders'];
+export const TAGS = ['watch', 'hike', 'animals', 'completed', 'archived', 'water park', 'event', 'food', 'trang', 'wonders', 'national park'];
