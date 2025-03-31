bc49eef6c266f69478f6d66175e5923cba7d9027
Ray Lim <rayjlim@yahoo.com>
2025-03-30 19:33:39 -0700

    fix: vitest configs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./setupTests.js",
-    include: ['src/**/*.{test,spec}.{js,jsx,ts,tsx}'],
-    exclude: ['node_modules', 'dist'],
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      all: true,
-      include: ['src/**/*.{js,jsx}'],
-      exclude: ['node_modules', 'dist'],
-    },
-    alias: {
-      'jest': 'vitest'
-    }
-  },
+  plugins: [react()]
 })
