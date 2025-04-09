572a3d79c81524b7fc90278d9d783b5e7ae67830
Ray Lim <rayjlim@yahoo.com>
2025-04-08 22:23:30 -0700

    fix: redo add LocalizationProvider for date handling
    Wrap the application with LocalizationProvider to enable date handling using MUI's date picker components. This change also includes the addition of necessary CSS files for styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import Navigation from './components/Navigation';
 import GoalsListPage from './views/GoalsListPage';
 import CompletedGoalsView from './views/CompletedGoalsView';
 import { BASENAME } from './constants';
-
+import './index.scss'
+import './App.css'
 function App() {
     return (
-        <BrowserRouter basename={BASENAME}>
-            <Navigation />
-            <Routes>
-                <Route path="/" element={<GoalsListPage />} />
-                <Route path="/completed" element={<CompletedGoalsView />} />
-            </Routes>
-        </BrowserRouter>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <BrowserRouter basename={BASENAME}>
+                <Navigation />
+                <Routes>
+                    <Route path="/" element={<GoalsListPage />} />
+                    <Route path="/completed" element={<CompletedGoalsView />} />
+                </Routes>
+            </BrowserRouter>
+        </LocalizationProvider >
     );
 }
 
